acdb2afa775b8885454712f6c2a5ea6e04ae232d
jallisson <jallissonn@hotmail.com>
2025-05-23 16:45:07 -0300

    Deploy new port

--- server/config/db.js
+++ server/config/db.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  port: process.env.DB_PORT || 4000,
+  port: process.env.DB_PORT || 10000,
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'seu_banco_de_dados',
   waitForConnections: true,
